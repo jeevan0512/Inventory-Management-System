f9e30c8178f83e8a135028149a2b5730cc4a3ac6
Jeevan R Shet <jeevanrshet0@gmail.com>
2025-06-10 19:30:09 +0530

    Update DBUtil.java

--- src/main/java/com/example/DBUtil.java
+++ src/main/java/com/example/DBUtil.java
@@ -1,4 +1,4 @@
-/*package com.example;
+package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -22,28 +22,3 @@ public class DBUtil {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
-*/
-package com.example;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class DBUtil {
-    private static final String URL = "jdbc:mysql://hopper.proxy.rlwy.net:32474/railway?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USER = "root";
-    private static final String PASS = "DqbhgwJBrvvGgHnLgAHcIXVKCLXghbnR";
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
-    }
-}
-
