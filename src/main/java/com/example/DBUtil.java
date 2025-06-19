62330bb446f8fe589b4ac9b74fe51462d82aece8
Jeevan R Shet <jeevanrshet0@gmail.com>
2025-06-19 15:39:36 +0530

    Update DBUtil.java

--- src/main/java/com/example/DBUtil.java
+++ src/main/java/com/example/DBUtil.java
@@ -1,14 +1,17 @@
-
- package com.example; 
+package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "mysql://root:DqbhgwJBrvvGgHnLgAHcIXVKCLXghbnR@hopper.proxy.rlwy.net:32474/railway";
-    private static final String USER = "root"; 
-    private static final String PASS = "DqbhgwJBrvvGgHnLgAHcIXVKCLXghbnR"; 
+    private static final String HOST = System.getenv("DB_HOST");       
+    private static final String PORT = System.getenv("DB_PORT");       
+    private static final String DB_NAME = System.getenv("DB_NAME");    
+    private static final String USER = System.getenv("DB_USER");       
+    private static final String PASS = System.getenv("DB_PASS");       
+
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME + "?useSSL=false&serverTimezone=UTC";
 
     static {
         try {
